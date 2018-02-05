0de2a2a4902765bf7807144bc98803e3e11088ee
onsmak <onsmak@igloonet.cz>
2018-02-05 12:53:36 +0100

    make async, defer as volatile class attributes

--- WebLoader/Nette/JavaScriptLoader.php
+++ WebLoader/Nette/JavaScriptLoader.php
@@ -2,6 +2,7 @@
 
 namespace WebLoader\Nette;
 
+use WebLoader\Compiler;
 use Nette\Utils\Html;
 
 /**
@@ -13,14 +14,60 @@ use Nette\Utils\Html;
 class JavaScriptLoader extends WebLoader
 {
 
+	/** @var bool */
+	private $async;
+
+	/** @var bool */
+	private $defer;
+
+	public function __construct(Compiler $compiler, $tempPath, $async = false, $defer = false)
+	{
+		parent::__construct($compiler, $tempPath);
+		$this->async = $async;
+		$this->defer = $defer;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isAsync()
+	{
+			return $this->async;
+	}
+
+	/**
+	 * @param bool $async
+	 */
+	public function setAsync($async)
+	{
+			$this->async = $async;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isDefer()
+	{
+			return $this->defer;
+	}
+
+	/**
+	 * @param bool $defer
+	 */
+	public function setDefer($defer)
+	{
+			$this->defer = $defer;
+	}
+
+
 	/**
 	 * Get script element
 	 * @param string $source
-	 * @return Html
+		 * @return Html
 	 */
 	public function getElement($source)
 	{
-		return Html::el("script", ['async' => TRUE, 'defer' => TRUE])->type("text/javascript")->src($source);
+		return Html::el("script", ['async' => $this->isAsync(), 'defer' => $this->isDefer()])->type("text/javascript")->src($source);
 	}
 
 }
